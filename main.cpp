86c7fbdd8cad4290408817a8bb7e33be28c9d392
ernerdgn <78610979+ernerdgn@users.noreply.github.com>
2025-01-28 15:45:53 +0300

    -update main.cpp
    -bmp media can be loaded

--- main.cpp
+++ main.cpp
@@ -1,37 +1,89 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int WIDTH = 640;
+const int HEIGHT = 480;
 
-int main(int argc, char* args[])
+SDL_Window* gWindow = NULL;
+
+SDL_Surface* gScreenSurface = NULL;
+
+SDL_Surface* gHelloWorld = NULL;
+
+bool init()
 {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
+	bool success = true;
 
-	if (!SDL_Init(SDL_INIT_VIDEO)) SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_Log("SDL initialization failed. SDL_ERROR: %s\n", SDL_GetError());
+		success = false;
+	}
 
 	else
 	{
-		window = SDL_CreateWindow("SDL Tutorial", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+		gWindow = SDL_CreateWindow("SDL Tutorial", WIDTH, HEIGHT, 0);
 
-		if (window == NULL) SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		if (gWindow == NULL)
+		{
+			SDL_Log("Window creation failed. SDL_ERROR: %s\n", SDL_GetError());
+			success = false;
+		}
+
+		else gScreenSurface = SDL_GetWindowSurface(gWindow);
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	bool success = true;
+
+	gHelloWorld = SDL_LoadBMP("PATH/TO/BMP.bmp");  //PATH/TO/BMP.bmp
+	if (gHelloWorld == NULL)
+	{
+		SDL_Log("Image loading is failed %s. SDL_ERROR: %s\n", "PATH/TO/BMP.bmp", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	SDL_DestroySurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	if (!init())
+	{
+		SDL_Log("Initialization failed.\n");
+	}
+	else
+	{
+		if (!loadMedia())
+		{
+			SDL_Log("Media load is failed.\n");
+		}
 		else
 		{
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillSurfaceRect(screenSurface, NULL, SDL_MapSurfaceRGB(screenSurface, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
-			SDL_Event e;
-			bool quit = false;
-			while (quit == false)
-			{
-				while (SDL_PollEvent(&e)) if (e.type == SDL_EVENT_QUIT) quit = true;
-			}
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+			SDL_UpdateWindowSurface(gWindow);
+
+			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_EVENT_QUIT) quit = true; } }
 		}
 	}
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	close();
 
 	return 0;
 }
